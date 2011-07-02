b9c8b322aa704594299898e694e87b3fae4a714c
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-07-02 08:50:14 -0300

    Lets fix this urlconf settings

--- settings.py
+++ settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'gradefisl.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, "templates"),
