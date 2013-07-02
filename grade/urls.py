a88bc68ef35362f2d532c4508a34fd26134c626c
Wille Marcel <wille@wille.blog.br>
2013-07-01 22:34:49 -0300

    making clips compatible to django 1.5

--- grade/urls.py
+++ grade/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 from views import TalkListView, TalkDetailView, IndexView, AuthorDetailView, ZoneDetailView, RoomDetailView, NowListView, DayTalkListView, AboutView, SearchTalkListView
 from django.views.generic import ListView
 from grade.models import Zone, Room, Talk
-from django.views.generic.simple import redirect_to
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('grade.views',
     url(r'^palestras/$', TalkListView.as_view(), name='talks'),
@@ -32,6 +32,6 @@ urlpatterns = patterns('grade.views',
     url(r'^agora/$', NowListView.as_view(), name='now'),
     url(r'^sobre/$', AboutView.as_view(), name='about'),
     url(r'^busca/$', SearchTalkListView.as_view()),
-    url(r'^code/$', redirect_to, {'url': 'https://github.com/willemarcel/gradefisl'}),
+    url(r'^code/$', RedirectView.as_view(url='https://github.com/willemarcel/gradefisl'), name='code'),
 
 )
