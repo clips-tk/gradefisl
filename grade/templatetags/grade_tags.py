47dbbcbee5da8503fd7a91e659fad45a52afb8eb
Wille Marcel <wille@wille.blog.br>
2013-09-27 09:03:06 -0300

    show hour only if there is at least one talk on that hour and add link to the authors detail page

--- grade/templatetags/grade_tags.py
+++ grade/templatetags/grade_tags.py
@@ -14,19 +14,22 @@ register = Library()
 def display_talks(day, hour):
     """ Show fisl talks given a day and hour """
 
-    return {'talks': Talk.objects.filter(date__contains=day.date).filter(date__contains=hour)}
+    return {'talks': Talk.objects.filter(date__contains=day.date) \
+    .filter(date__contains=hour), 'hour': hour}
 
 @register.inclusion_tag('tags/display_talks.html')
 def display_talks_zone(day, hour, zone):
     """ Show fisl talks given a day, hour and zone """
 
-    return {'talks': Talk.objects.filter(zone=zone, date__contains=day.date).filter(date__contains=hour)}
+    return {'talks': Talk.objects.filter(zone=zone, date__contains=day.date) \
+    .filter(date__contains=hour), 'hour': hour}
 
 @register.inclusion_tag('tags/display_talks.html')
 def display_talks_room(day, hour, room):
     """ Show fisl talks given a day, hour and room """
 
-    return {'talks': Talk.objects.filter(room=room, date__contains=day.date).filter(date__contains=hour)}
+    return {'talks': Talk.objects.filter(room=room, date__contains=day.date) \
+    .filter(date__contains=hour), 'hour': hour}
 
 @register.inclusion_tag('tags/display_talks.html')
 def display_talks_next_hour(day, next_hour):
