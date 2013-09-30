8f927065e6802ea294dd6e55717a3944efdd8533
Wille Marcel <wille@wille.blog.br>
2013-09-30 07:41:22 -0300

    add django admin configuration

---
+++ grade/admin.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from grade.models import Talk
+from grade.models import Room
+from grade.models import Author
+from grade.models import Zone
+
+
+class TalkAdmin(admin.ModelAdmin):
+    fields = ['title', 'abstract', 'date', 'authors', 'zone', 'room', 'language']
+    list_display = ('title', 'date', 'zone', 'room')
+    list_filter = ['date', 'zone', 'room', 'language']
+    search_fields = ('title', 'abstract', 'authors')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ['name', 'bio']
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class RoomAdmin(admin.ModelAdmin):
+    fields = ['name', 'capacity', 'translation']
+    search_fields = ('name',)
+
+
+class ZoneAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+admin.site.register(Talk, TalkAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Zone, ZoneAdmin)
