3c29f4b2fa81a57d2cb056b4c9eab694977a4e6b
Douglas Soares de Andrade <contato@douglasandrade.com>
2013-10-04 17:32:39 -0300

    Removing twitterauth as it has to be redone

--- twitterauth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('twitterauth.views',
-    url(r'^login/?$', 'twitter_login', name='login'),
-    url(r'^logout/?$', 'twitter_logout', name='logout'),
-    url(r'^login/authenticated/?$', 'twitter_authenticated',
-        name='authenticated'),
-)
